1145e7d485a294d96989934050a22d9d27235f4e
Christie <me.mcbeukman@gmail.com>
2017-08-23 18:38:39 +0200

    View

--- Schedule/Services/IDataAccessSupplier.cs
+++ Schedule/Services/IDataAccessSupplier.cs
@@ -1,12 +1,18 @@
-﻿using System;
+﻿using Schedule.Model;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Schedule.Services
 {
-    interface IDataAccessSupplier
+    public interface IDataAccessSupplier
     {
+        ObservableCollection<Suppliier> GetSuppliers();
+        void CreateSupplier(Suppliier Sup);
+        void DeleteSupplier(Suppliier Sup);
+        void UpdateSupplier(Suppliier NewSup, Suppliier OldSup);
     }
 }
--- Schedule/ViewModel/ViewModelLocator.cs
+++ Schedule/ViewModel/ViewModelLocator.cs
@@ -1,20 +1,8 @@
-/*
-  In App.xaml:
-  <Application.Resources>
-      <vm:ViewModelLocator xmlns:vm="clr-namespace:Schedule"
-                           x:Key="Locator" />
-  </Application.Resources>
-  
-  In the View:
-  DataContext="{Binding Source={StaticResource Locator}, Path=ViewModelName}"
-
-  You can also use Blend to do all this with the tool's support.
-  See http://www.galasoft.ch/mvvm
-*/
 
 using GalaSoft.MvvmLight;
 using GalaSoft.MvvmLight.Ioc;
 using Microsoft.Practices.ServiceLocation;
+using Schedule.Services;
 
 namespace Schedule.ViewModel
 {
@@ -31,18 +19,9 @@ namespace Schedule.ViewModel
         {
             ServiceLocator.SetLocatorProvider(() => SimpleIoc.Default);
 
-            ////if (ViewModelBase.IsInDesignModeStatic)
-            ////{
-            ////    // Create design time view services and models
-            ////    SimpleIoc.Default.Register<IDataService, DesignDataService>();
-            ////}
-            ////else
-            ////{
-            ////    // Create run time view services and models
-            ////    SimpleIoc.Default.Register<IDataService, DataService>();
-            ////}
-
             SimpleIoc.Default.Register<MainViewModel>();
+            SimpleIoc.Default.Register<SupplierViewModel>();
+            SimpleIoc.Default.Register<IDataAccessSupplier, DataAccessSupplier>();
         }
 
         public MainViewModel Main
@@ -52,7 +31,14 @@ namespace Schedule.ViewModel
                 return ServiceLocator.Current.GetInstance<MainViewModel>();
             }
         }
-        
+
+        public SupplierViewModel SupplierVM
+        { get
+            {
+                return ServiceLocator.Current.GetInstance<SupplierViewModel>();
+            }
+        }
+
         public static void Cleanup()
         {
             // TODO Clear the ViewModels
